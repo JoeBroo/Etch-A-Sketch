9358e8b1f110b1c8582daf79691b85158cb7b8e8
JoeBroo <jdoe40630@gmail.com>
2023-06-01 12:34:03 -0400

    add create square function and event listeners

---
+++ app.js
@@ -0,0 +1,27 @@
+const mainDiv = document.querySelector('.main');
+const divs = document.querySelectorAll('.div');
+const addSquare = document.querySelector('.add-square');
+
+//Adds event listener for test squares
+divs.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+       item.style.backgroundColor = 'red'; 
+    })
+})
+
+//Adds square when button is clicked
+addSquare.addEventListener('click', play);
+
+//Creates and add square
+function play() {
+    const createdDiv = document.createElement('div');
+    createdDiv.style.backgroundColor = 'whitesmoke';
+    createdDiv.classList.add('div');
+    mainDiv.appendChild(createdDiv);
+
+    //Adds event listener to added square(s)
+    createdDiv.addEventListener('mouseover', () => {
+        createdDiv.style.backgroundColor = 'red';
+        createdDiv.style.border = '2px solid white';
+    })
+}
