8e4f66011d336cc27211549f6194eda53d80152b
JoeBroo <jdoe40630@gmail.com>
2023-06-09 00:16:25 -0400

    add option to set grid size

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ function createSquare() {
     const createdDiv = document.createElement('div');
     createdDiv.style.backgroundColor = 'whitesmoke';
     createdDiv.classList.add('div');
+    createdDiv.style.display = 'flex';
+    createdDiv.style.flex = '1';
     mainDiv.appendChild(createdDiv);
 
     //Adds event listener to added square(s)
@@ -29,9 +31,31 @@ function createSquare() {
     })
 }
 
-randomColor.addEventListener('click', randomizeColor);
-
-//Alerts user. Will cycle through hex color values.
+//Alerts user when randomize color button is clicked.
 function randomizeColor() {
     alert('You randomized the color! ');
+}
+
+//Asks for gird size on button click.
+gridBtn.addEventListener('click', setGridSize);
+
+function setGridSize() {
+    let size = prompt('How many squares would you like to add?');
+
+    //Adds 16 squares.
+    if (size == 16) {
+        for (let i = 0; i < size; i++) {
+            createSquare();
+        }
+
+        //Adds 256 squares.
+    } else if (size == 256) {
+        for (let i = 0; i < size; i++) {
+            createSquare();
+        }
+
+        //If choice is not one of the above, let user know their options.
+    } else if (size !== 16 || 256) {
+        alert('You may choose 16 or 256. ');
+    }
 }
