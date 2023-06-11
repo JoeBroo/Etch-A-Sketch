2510e3b53762914485ae2d47625cf508f704616d
JoeBroo <jdoe40630@gmail.com>
2023-06-10 22:05:59 -0400

    change border and background color on mouse enter.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const mainDiv = document.querySelector('.main');
-const divs = document.querySelectorAll('.div');
+// const divs = document.querySelectorAll('.div');
 const addSquare = document.querySelector('.add-square');
 const randomColor = document.querySelector('.random-color');
 const gridBtn = document.querySelector('.grid-button');
 
 //Adds event listener for test squares
-divs.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        let randomColorChoice = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        item.style.backgroundColor = randomColorChoice;
-    })
-})
+// divs.forEach((item) => {
+//     item.addEventListener('mouseover', () => {
+//         let randomColorChoice = '#' + Math.floor(Math.random() * 16777215).toString(16);
+//         item.style.backgroundColor = randomColorChoice;
+//     })
+// })
 
 //Adds square when button is clicked
 addSquare.addEventListener('click', createSquare);
@@ -19,15 +19,21 @@ addSquare.addEventListener('click', createSquare);
 function createSquare() {
     const createdDiv = document.createElement('div');
     createdDiv.style.backgroundColor = 'whitesmoke';
+    // createdDiv.style.border = '2px solid black';
     createdDiv.classList.add('div');
     createdDiv.style.display = 'flex';
     createdDiv.style.flex = '1';
     mainDiv.appendChild(createdDiv);
 
     //Adds event listener to added square(s)
-    createdDiv.addEventListener('mouseover', () => {
-        createdDiv.style.backgroundColor = 'red';
+    createdDiv.addEventListener('mouseenter', () => {
+        createdDiv.style.backgroundColor = 'rgb(231,40,93)';
         createdDiv.style.border = '2px solid white';
+    }) 
+    
+    createdDiv.addEventListener('mouseleave', () => {
+        createdDiv.style.backgroundColor = 'black';
+        // createdDiv.style.border = '2px solid whitesmoke';
     })
 }
 
@@ -42,20 +48,9 @@ gridBtn.addEventListener('click', setGridSize);
 function setGridSize() {
     let size = prompt('How many squares would you like to add?');
 
-    //Adds 16 squares.
-    if (size == 16) {
+    if (size === size) {
         for (let i = 0; i < size; i++) {
             createSquare();
         }
-
-        //Adds 256 squares.
-    } else if (size == 256) {
-        for (let i = 0; i < size; i++) {
-            createSquare();
-        }
-
-        //If choice is not one of the above, let user know their options.
-    } else if (size !== 16 || 256) {
-        alert('You may choose 16 or 256. ');
     }
 }
