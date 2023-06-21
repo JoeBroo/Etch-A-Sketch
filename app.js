44e74f02a7ccf00605563c7f4c5836ed541f3451
JoeBroo <jdoe40630@gmail.com>
2023-06-20 20:11:31 -0400

    Add random color function

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
 const mainDiv = document.querySelector('.main');
-// const divs = document.querySelectorAll('.div');
 const addSquare = document.querySelector('.add-square');
 const randomColor = document.querySelector('.random-color');
 const gridBtn = document.querySelector('.grid-button');
 
-//Adds event listener for test squares
-// divs.forEach((item) => {
-//     item.addEventListener('mouseover', () => {
-//         let randomColorChoice = '#' + Math.floor(Math.random() * 16777215).toString(16);
-//         item.style.backgroundColor = randomColorChoice;
-//     })
-// })
-
 //Adds square when button is clicked
-addSquare.addEventListener('click', createSquare);
+addSquare.addEventListener('click', startGrid);
 
+function startGrid() {
+    const text = ['You', 'added', 'squares'];
+    for (let i = 0; i < text.length; i += 1) {
+        const div2 = document.createElement('div');
+        div2.classList.add('div2');
+        div2.textContent = text[i];
+        mainDiv.appendChild(div2);
+    }
+}
 //Creates and add square
-function createSquare() {
+function play() {
     const createdDiv = document.createElement('div');
     createdDiv.style.backgroundColor = 'whitesmoke';
-    // createdDiv.style.border = '2px solid black';
     createdDiv.classList.add('div');
-    createdDiv.style.display = 'flex';
-    createdDiv.style.flex = '1';
     mainDiv.appendChild(createdDiv);
 
     //Adds event listener to added square(s)
     createdDiv.addEventListener('mouseenter', () => {
         createdDiv.style.backgroundColor = 'rgb(231,40,93)';
-        createdDiv.style.border = '2px solid white';
     }) 
     
     createdDiv.addEventListener('mouseleave', () => {
         createdDiv.style.backgroundColor = 'black';
-        // createdDiv.style.border = '2px solid whitesmoke';
     })
-}
 
-//Alerts user when randomize color button is clicked.
-function randomizeColor() {
-    alert('You randomized the color! ');
+    randomColor.addEventListener('click', () => {
+        let randomColorChoice = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        createdDiv.addEventListener('mouseleave', () => {
+            createdDiv.style.backgroundColor = randomColorChoice;
+        })
+    })
 }
 
 //Asks for gird size on button click.
@@ -48,9 +45,18 @@ gridBtn.addEventListener('click', setGridSize);
 function setGridSize() {
     let size = prompt('How many squares would you like to add?');
 
-    if (size === size) {
+    if (size >= 1) {
         for (let i = 0; i < size; i++) {
-            createSquare();
+            play();
         }
+    } else if (size <= 0) {
+        alert('Enter a number between 1 and 1000');
+    } else if (size >= 300) {
+        alert('Enter a number between 1 and 1000');
     }
+}
+
+for (let i = 0; i < 257; i++) {
+    // console.log(i);
+    play();
 }
