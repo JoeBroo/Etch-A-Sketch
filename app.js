71365d71a5202f2940fd91bc716479220b28b0e0
JoeBroo <jdoe40630@gmail.com>
2023-07-03 22:03:55 -0400

    Refactor setGridSize and startGrid code.

--- app.js
+++ app.js
@@ -4,17 +4,10 @@ const randomColor = document.querySelector('.random-color');
 const gridBtn = document.querySelector('.grid-button');
 
 //Adds square when button is clicked
-addSquare.addEventListener('click', startGrid);
+addSquare.addEventListener('click', () => {
+    alert('You clicked add square! ');
+});
 
-function startGrid() {
-    const text = ['You', 'added', 'squares'];
-    for (let i = 0; i < text.length; i += 1) {
-        const div2 = document.createElement('div');
-        div2.classList.add('div2');
-        div2.textContent = text[i];
-        mainDiv.appendChild(div2);
-    }
-}
 //Creates and add square
 function play() {
     const createdDiv = document.createElement('div');
@@ -22,7 +15,7 @@ function play() {
     createdDiv.classList.add('div');
     mainDiv.appendChild(createdDiv);
 
-    //Adds event listener to added square(s)
+    //Adds red color to squares on mouse enter and change to black when mouse leaves.
     createdDiv.addEventListener('mouseenter', () => {
         createdDiv.style.backgroundColor = 'rgb(231,40,93)';
     }) 
@@ -31,6 +24,7 @@ function play() {
         createdDiv.style.backgroundColor = 'black';
     })
 
+    //Add random color to squares when button clicked.
     randomColor.addEventListener('click', () => {
         let randomColorChoice = '#' + Math.floor(Math.random() * 16777215).toString(16);
         createdDiv.addEventListener('mouseleave', () => {
@@ -43,20 +37,27 @@ function play() {
 gridBtn.addEventListener('click', setGridSize);
 
 function setGridSize() {
-    let size = prompt('How many squares would you like to add?');
+    let row = prompt('How many rows would you like? ');
+    let column = prompt('How many columns would you like? ');
+    let sum = row * column;
+    console.log(`The sum of ${row} and ${column} is: ${sum}`);
 
-    if (size >= 1) {
-        for (let i = 0; i < size; i++) {
-            play();
+    if (sum <= 0 || sum > 10000) {
+        alert('Please enter a number between 1 and 100')
+    } else {
+        for (let i = 0; i < sum; i++) {
+            const div2 = document.createElement('div');
+            div2.textContent = "I'm alive! ";
+            div2.classList.add('div2');
+            mainDiv.appendChild(div2);
         }
-    } else if (size <= 0) {
-        alert('Enter a number between 1 and 1000');
-    } else if (size >= 300) {
-        alert('Enter a number between 1 and 1000');
     }
 }
 
-for (let i = 0; i < 257; i++) {
-    // console.log(i);
-    play();
+function startGrid() {
+    for (let i = 0; i < 257; i++) {
+        play();
+    }
 }
+
+startGrid();
